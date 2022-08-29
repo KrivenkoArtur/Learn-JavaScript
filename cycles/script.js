c8ffc5e35b9bbf8c5068d27c7f8b71b15cb21bb9
Artur Krivenko <88277246+KrivenkoArtur@users.noreply.github.com>
2022-08-29 19:47:57 +0300

    code exercise with cicles and update firstApp

---
+++ cycles/script.js
@@ -0,0 +1,11 @@
+function secondTask() {
+    // Пишем решение вот тут
+    for (let i = 21; i <= 10; i--) {
+        if (i === 13) {
+            break;
+        }
+        console.log(i);
+    } 
+};
+
+secondTask();
